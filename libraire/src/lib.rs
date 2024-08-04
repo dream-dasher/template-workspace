a266076e082d767cbca16e54338f24da906bc829
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-08-04 13:47:45 -0500

    Tests: added doc tests

--- libraire/src/lib.rs
+++ libraire/src/lib.rs
@@ -1,15 +1,50 @@
 //! Example Library in an Example Workspace
 
-/// Adds two numbers together.
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 /// Prints "Hi!" to screen.
 pub fn say_hi() {
     println!("Hi!");
 }
 
+/// First line is a short summary describing function.
+///
+/// The next lines present detailed documentation. Code blocks start with
+/// triple backquotes and have implicit `fn main()` inside
+/// and `extern crate <cratename>`. Assume we're testing `libraire` crate:
+///
+/// ```
+/// let result = libraire::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: u64, b: u64) -> u64 {
+    a + b
+}
+
+/// Usually doc comments may include sections "Examples", "Panics" and "Failures".
+///
+/// The next function divides two numbers.
+///
+/// # Examples
+///
+/// ```
+/// let result = libraire::div(10, 2);
+/// assert_eq!(result, 5);
+/// ```
+///
+/// # Panics
+///
+/// The function panics if the second argument is zero.
+///
+/// ```rust,should_panic
+/// // panics on division by zero
+/// libraire::div(10, 0);
+/// ```
+pub fn div(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    }
+
+    a / b
+}
 /// Repeats an immutable function multiple times
 ///
 /// Function does not directly take inputs.
