56b2c446338c3079af947872878ce5f6208d00ab
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-08-11 05:13:55 -0700

    Feat: simple CLI interace

--- cli_bin/src/main.rs
+++ cli_bin/src/main.rs
@@ -1,10 +1,30 @@
-//! An Example Script using Example Library in Example Workspace
+#![feature(error_generic_member_access)]
+//! An Example Binary (CLI app) using Example Library in Example Workspace
+//!
 
+mod error;
+mod support_tracing;
+
+use clap::Parser;
+use error::Result;
 use libraire_lib::{repeat_function, utility::say_hi};
+// use rename_files::{app, error::Result, logging, Args};
 
-fn main() {
+fn main() -> Result<()> {
+        support_tracing::tracing_subscribe_boilerplate("warn");
+        let args = Args::parse();
         println!("Script 1, running...");
-        repeat_function(say_hi, 7);
+        repeat_function(say_hi, args.repeats);
+        Ok(())
+}
+
+/// CLI Args
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+pub struct Args {
+        /// Ints, xp.
+        #[arg(short, long)]
+        repeats: u8,
 }
 
 #[cfg(test)]
