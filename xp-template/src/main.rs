08358f0a2b68816adba3631933f324b18565f628
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-08-31 00:33:49 -0700

    Feat: args layout for template exploration

--- xp-template/src/main.rs
+++ xp-template/src/main.rs
@@ -12,86 +12,48 @@ use tracing::{event, info, Level};
 struct Args {
         /// Some kinda mode
         #[arg(value_enum)]
-        speed: Mode,
-
-        #[arg(default_value_t = 2020)]
-        /// Network port: Optional (w/ default)
-        #[arg(value_parser = clap::value_parser!(u16).range(1..))]
-        port: u16,
+        templater: TemplateSystem,
 
         /// Name: Optional
         name: Option<String>,
 
-        /// flag_Repeats: Optional
-        #[arg(short, long)]
-        repeats: Option<u8>,
-
-        /// Sets a custom config file
+        /// Optional Path to a template file
         #[arg(short, long, value_name = "FILE")]
-        config: Option<PathBuf>,
-
-        /// Turn debugging information on
-        #[arg(short, long, action = clap::ArgAction::Count)]
-        debug: u8,
+        template_path: Option<PathBuf>,
 
         /// You can repeat me
         #[arg(short, long, action = clap::ArgAction::Count)]
         verbose: u8,
-
-        /// Some sort of sub-command
-        #[command(subcommand)]
-        subcommand: Option<SubCommands>,
 }
+/// Templating Crate to use
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
-enum Mode {
-        /// Run swiftly
-        Fast,
-        /// Crawl slowly but steadily
-        ///
-        /// This paragraph is ignored because there is no long help text for possible values.
-        Slow,
-}
-
-#[derive(Subcommand, Debug)]
-enum SubCommands {
-        /// does testing things
-        LightSwitch {
-                /// lists test values
-                #[arg(short, long)]
-                toggle: bool,
-        },
+enum TemplateSystem {
+        /// no templating system
+        None,
+        /// minijinja
+        MiniJinja,
+        /// LiquidRust
+        LiquidRust,
+        /// Handlebars
+        Handlebars,
 }
 
 fn main() -> Result<()> {
-        support_tracing::tracing_subscribe_boilerplate("info");
-        tracing::event!(Level::DEBUG, "Script 1, starting...");
         let args = Args::parse();
-        println!("Script 1, running... {}", args.repeats.unwrap_or(0));
+        match &args.verbose {
+                0 => support_tracing::tracing_subscribe_boilerplate("error"),
+                1 => support_tracing::tracing_subscribe_boilerplate("warn"),
+                2 => support_tracing::tracing_subscribe_boilerplate("info"),
+                3 => support_tracing::tracing_subscribe_boilerplate("debug"),
+                _ => support_tracing::tracing_subscribe_boilerplate("trace"),
+        }
+        tracing::event!(Level::DEBUG, "Script 1, starting...");
         info!(?args);
-
-        match &args.speed {
-                Mode::Fast => {
-                        println!("Hare");
-                }
-                Mode::Slow => {
-                        println!("Tortoise");
-                }
-        }
-
-        match &args.subcommand {
-                Some(SubCommands::LightSwitch { toggle: list_bool }) => {
-                        event!(Level::INFO, "Subcommand Provided.");
-                        match list_bool {
-                                true => event!(Level::INFO, "Toggle is set to  ^^true^^."),
-                                false => event!(Level::INFO, "Toggle is set to  v-false-v."),
-                        }
-                }
-                None => event!(Level::WARN, "SubCommand not given."),
-        }
 
         Ok(())
 }
 
+// /////////////////////////////////////////////////////////////////////////////////////// //
 /// EARLY_DEV: non-specific error & result types for use while exploring new code.
 mod error {
         pub type Result<T> = core::result::Result<T, Error>;
