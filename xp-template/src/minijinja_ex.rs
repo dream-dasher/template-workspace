02272e93baa93681dfc1c6e3c52bad3777804fdc
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-09-19 17:46:12 -0400

    chore: name clarity

--- xp-template/src/minijinja_ex.rs
+++ xp-template/src/minijinja_ex.rs
@@ -4,8 +4,8 @@ use crate::error::Result;
 pub fn mini_jinja_example() -> Result<()> {
         use minijinja::{context, Environment};
         let mut env = Environment::new();
-        env.add_template("hello", "Hello {{ name }}!").unwrap();
-        let tmpl = env.get_template("hello").unwrap();
+        env.add_template("hello_template", "Hello {{ name }}!").unwrap();
+        let tmpl = env.get_template("hello_template").unwrap();
         println!("{}", tmpl.render(context!(name => "John")).unwrap());
         Ok(())
 }
