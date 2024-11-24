2dd3e965116a2b62303914de0f462e0b67eaba1e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-24 21:34:11 +0100

    wip: threads

--- samples_tracing/src/main.rs
+++ samples_tracing/src/main.rs
@@ -2,23 +2,26 @@
 //!
 //! clear; RUST_LOG=trace carrbn samples_tracing  bb ccc dddd
 
-use std::io::Read;
+use std::{io::Read, thread};
 
-use tracing::{Level, debug, error, info, span, trace, warn};
+use tracing::{Level, debug, error, info, info_span, span, trace, warn};
 fn main() {
         tracing_subscriber::fmt::init();
         let span = span!(Level::INFO, "main");
         let _guard = span.enter();
         for file in std::env::args().skip(1) {
-                let span = span!(Level::INFO, "file", fname = %file);
-                let _guard = span.enter();
-                info!("opening the file");
-                // let mut file = std::fs::File::open(file).unwrap();
-                info!("reading file contents");
-                // let mut bytes = Vec::new();
-                // file.read_exact(&mut bytes).unwrap();
-                info!("parsing");
-                // ..
-                info!("done with file");
+                thread::spawn(move || {
+                        let span = info_span!("file", fname = %file);
+                        let _guard = span.enter();
+                        info!("opening the file");
+                        // let mut file = std::fs::File::open(file).unwrap();
+                        info!("reading file contents");
+                        // let mut bytes = Vec::new();
+                        // file.read_exact(&mut bytes).unwrap();
+                        info!(bytes = 0, "parsing");
+                        // ..
+                        info!("done with file");
+                });
         }
+        thread::sleep(std::time::Duration::from_secs(1));
 }
