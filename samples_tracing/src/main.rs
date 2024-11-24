f038a348c266ef7e3a08d38f3f188648eb2f6b5d
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-24 21:02:22 +0100

    wip - tracing skip args

--- samples_tracing/src/main.rs
+++ samples_tracing/src/main.rs
@@ -2,14 +2,15 @@ use std::io::Read;
 
 use tracing::{Level, debug, error, info, span, trace, warn};
 fn main() {
-        for file in std::env::args() {
-                let span = span!(Level::INFO, "file");
+        tracing_subscriber::fmt::init();
+        for file in std::env::args().skip(1) {
+                let span = span!(Level::INFO, "file", fname = %file);
                 let _guard = span.enter();
                 info!("opening the file");
-                let mut file = std::fs::File::open(file).unwrap();
+                // let mut file = std::fs::File::open(file).unwrap();
                 info!("reading file contents");
-                let mut bytes = Vec::new();
-                file.read_exact(&mut bytes).unwrap();
+                // let mut bytes = Vec::new();
+                // file.read_exact(&mut bytes).unwrap();
                 info!("parsing");
                 // ..
                 info!("done with file");
