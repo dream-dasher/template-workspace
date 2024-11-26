a10bba1076113528464852615f3e95300cb5df56
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-26 01:16:35 +0100

    wip: tracing with thread and instrumentation

--- samples_tracing/src/main.rs
+++ samples_tracing/src/main.rs
@@ -14,40 +14,29 @@
 //!
 //! clear; RUST_LOG=trace carrbn samples_tracing  a bb ccc dddd
 
-use std::{io::Read, thread};
+use core::fmt;
+use std::{io::Read, path::PathBuf, thread};
 
 #[derive(Debug)]
-struct Foo {
+struct Foo
+{
         a: bool,
         b: u32,
 }
 
 use tracing::{Level, debug, error, info, info_span, span, trace, warn};
-fn main() {
+fn main()
+{
         tracing_subscriber::fmt::init();
-        let mut handles = vec![];
+
         let span = span!(Level::INFO, "main",);
         let _guard = span.enter();
+        let mut handles = vec![];
         info!(args = ?std::env::args(), "about to start file loop");
         for file in std::env::args().skip(1) {
-                let span_clone = span.clone();
-                let handle = thread::spawn(move || {
-                        let _guard = span_clone.enter();
-                        let span = info_span!("file", fname = %file);
-                        let _guard = span.enter();
-                        warn!(parent: None, "opening the file");
-                        // let mut file = std::fs::File::open(file).unwrap();
-                        info!("reading file contents");
-                        // let mut bytes = Vec::new();
-                        // file.read_exact(&mut bytes).unwrap();
-                        info!(bytes = 0, "parsing");
-                        // ..
-                        #[expect(clippy::disallowed_names)]
-                        let foo: Foo = Foo { a: false, b: 12 };
-                        info!(parsed = ?foo, "done with file");
-                });
-                handles.push(handle);
+                handles.push(std::thread::spawn(move || on_thread(file)));
         }
+
         let span = span!(Level::INFO, "joining");
         let _guard = span.enter();
         for handle in handles {
@@ -57,4 +46,22 @@ fn main() {
                 handle.join().expect("thread join error");
                 debug!("thread joined");
         }
+}
+
+#[tracing::instrument]
+fn on_thread<PATH>(file: PATH)
+        where PATH: Into<PathBuf>+std::fmt::Display+fmt::Debug
+{
+        let span = info_span!("file", fname = %file);
+        let _guard = span.enter();
+        warn!(parent: None, fname = %file, "opening the file");
+        // let mut file = std::fs::File::open(file).unwrap();
+        info!("reading file contents");
+        // let mut bytes = Vec::new();
+        // file.read_exact(&mut bytes).unwrap();
+        info!(bytes = 0, "parsing");
+        // ..
+        #[expect(clippy::disallowed_names)]
+        let foo: Foo = Foo { a: false, b: 12 };
+        info!(parsed = ?foo, "done with file");
 }
