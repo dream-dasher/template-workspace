4f9122fdb132bbae12cbee93f07c59e0e9a911d1
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-26 15:21:29 +0100

    feat: playing with RefCell

--- samples_sync/src/main.rs
+++ samples_sync/src/main.rs
@@ -1,11 +1,59 @@
 //! Playing with sync and relateed objects
 
-use std::{sync::{Arc, Barrier},
-          thread};
+use tracing::{Level, event, instrument};
 
 fn main()
 {
-        barrier_example(10, 20);
+        tracing_subscriber::fmt::init();
+
+        // barrier_example(10, 20);
+        refcell_example();
+}
+
+#[instrument]
+fn refcell_example()
+{
+        use std::{cell::{RefCell, RefMut},
+                  collections::HashMap,
+                  rc::Rc};
+
+        event!(Level::INFO, "create shared map");
+        let shared_map: Rc<RefCell<_>> = Rc::new(RefCell::new(HashMap::new()));
+        // Create a new block to limit the scope of the dynamic borrow
+        {
+                event!(Level::DEBUG, "mutable borrow of map");
+                let mut map: RefMut<'_, _> = shared_map.borrow_mut();
+                map.insert("A", 1);
+                map.insert("BB", 4);
+                map.insert("CCC", 9);
+                map.insert("DDDD", 16);
+        }
+
+        // Note that if we had not let the previous borrow of the cache fall out
+        // of scope then the subsequent borrow would cause a dynamic thread panic.
+        // This is the major hazard of using `RefCell`.
+        let total: i32 = shared_map.borrow().values().sum();
+        event!(Level::INFO, "total: {total}");
+        event!(Level::INFO, "map: {:?}", shared_map.borrow());
+
+        // Create a new block to limit the scope of the dynamic borrow
+        {
+                event!(Level::DEBUG, "mutable borrow of map");
+                let mut map: RefMut<'_, _> = shared_map.borrow_mut();
+                map.insert("EEEEE", 25);
+                map.insert("FFF_FFF", 36);
+                map.insert("GGG_G_GGG", 49);
+                map.insert("HHHH_HHHH", 64);
+        }
+
+        // Note that if we had not let the previous borrow of the cache fall out
+        // of scope then the subsequent borrow would cause a dynamic thread panic.
+        // This is the major hazard of using `RefCell`.
+        event!(Level::INFO, "old total: {total}");
+        event!(Level::INFO, "old map: {:?}", shared_map.borrow());
+        let total: i32 = shared_map.borrow().values().sum();
+        event!(Level::INFO, "total: {total}");
+        event!(Level::INFO, "map: {:?}", shared_map.borrow());
 }
 
 /// Example use of barrier.
@@ -13,8 +61,12 @@ fn main()
 ///
 /// ## Note
 /// if `to_generate` % `to_wait_for` != 0 then the function will not terminate.
+#[instrument]
 fn barrier_example(to_wait_for: usize, to_generate: usize)
 {
+        use std::{sync::{Arc, Barrier},
+                  thread};
+
         let n = to_generate;
         let mut handles = Vec::with_capacity(n);
         let barrier = Arc::new(Barrier::new(to_wait_for));
