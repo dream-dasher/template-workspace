c584809e862c9faceb4e1e332ab0a1208887662e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-29 16:35:26 +0100

    docs(ex): minor refcell code
    Improved use of tracing.

--- samples_mutability/src/main.rs
+++ samples_mutability/src/main.rs
@@ -1,36 +1,90 @@
 //! Playing mutability
+//!
+//! clear; RUST_LOG=samples_mutability=trace carrbn samples_mutability
 
-use tracing::{Level, event, instrument, span};
+use std::cell::RefCell;
+
+use tracing::{Level, debug, error, event, info, instrument, level_filters::LevelFilter, span, trace, warn};
+use tracing_subscriber::{EnvFilter, prelude::*};
+use tracing_tree::time::Uptime;
 
 fn main()
 {
-        tracing_subscriber::fmt::init();
-        let vectorio = vec![1, 2, 3, 4, 5];
-        let vectorio_reference_1 = &vectorio;
-        let vectorio_reference_2 = &vectorio;
-        event!(Level::INFO, ?vectorio);
-        event!(Level::INFO, ?vectorio_reference_1);
-        event!(Level::INFO, ?vectorio_reference_2);
+        tracing_subscriber::Registry::default()
+                .with(
+                        tracing_tree::HierarchicalLayer::new(2), // .with_timer(Uptime::default())
+                                                                 // .with_span_modes(true),
+                                                                 // .with_indent_lines(true)
+                )
+                .with(EnvFilter::builder()
+                        .with_default_directive(LevelFilter::ERROR.into())
+                        .from_env_lossy())
+                .init();
 
-        let mut vectart = vec![5, 4, 3, 2, 1];
-        let mut vectart_mut_reference_1 = &mut vectart;
-        // let vectart_mut_reference_2 = &mut vectart;
-        // event!(Level::INFO, ?vectart);
-        event!(Level::INFO, ?vectart_mut_reference_1);
-        // event!(Level::INFO, ?vectart_mut_reference_2);
-        let vectart_mut_reference_1_mutref = &mut vectart_mut_reference_1;
-        event!(Level::INFO, ?vectart_mut_reference_1_mutref);
-        event!(Level::INFO, ?vectart_mut_reference_1);
-        // event!(Level::INFO, ?vectart_mut_reference_1_mutref);
-        event!(Level::INFO, ?vectart);
+        let span = span!(Level::INFO, "View-Only Multi-Borrow");
+        {
+                let _guard = span.enter();
+                let vectorio = vec![1, 2, 3, 4, 5];
+                let vectorio_reference_1 = &vectorio;
+                let vectorio_reference_2 = &vectorio;
+                debug!(?vectorio);
+                debug!(?vectorio_reference_1);
+                debug!(?vectorio_reference_2);
+        }
 
-        let mut vectaline = vec![2, 1, 5, 1, 2];
-        let vectaline_taker_1 = vectaline;
-        // let vectaline_taker_2 = vectaline;
-        // event!(Level::INFO, ?vectaline);
-        event!(Level::INFO, ?vectaline_taker_1);
-        // event!(Level::INFO, ?vectaline_taker_2);
-        // let vectaline_taker_2 = vectaline;
+        let span = span!(Level::INFO, "Mutable-Access Exclusive-Borrow");
+        {
+                let _guard = span.enter();
+                let mut vectart = vec![5, 4, 3, 2, 1];
+                let mut vectart_mut_reference_1 = &mut vectart;
+                // let vectart_mut_reference_2 = &mut vectart;
+                // debug!(?vectart);
+                debug!(?vectart_mut_reference_1);
+                // debug!(?vectart_mut_reference_2);
+                let vectart_mut_reference_1_mutref = &mut vectart_mut_reference_1;
+                debug!(?vectart_mut_reference_1_mutref);
+                debug!(?vectart_mut_reference_1);
+                // debug!(?vectart_mut_reference_1_mutref);
+                debug!(?vectart);
+        }
+
+        let span = span!(Level::INFO, "Responsibility Transfer, Exclusive");
+        {
+                let _guard = span.enter();
+                let mut vectaline = vec![2, 1, 5, 1, 2];
+                let vectaline_taker_1 = vectaline;
+                // let vectaline_taker_2 = vectaline;
+                // debug!(?vectaline);
+                debug!(?vectaline_taker_1);
+                // debug!(?vectaline_taker_2);
+                // let vectaline_taker_2 = vectaline;
+        }
+
+        let span = span!(Level::INFO, "RefCell Exploration...");
+        {
+                let _guard = span.enter();
+                let vecto = vec!['a', 'b', 'c', 'd', 'e'];
+                let vecto_refcell = RefCell::new(vecto);
+                let span = span!(Level::INFO, "RefCell -- Immutable Borrow, Multiple");
+                {
+                        let _guard = span.enter();
+                        let ref_borrow_a = vecto_refcell.borrow();
+                        let ref_borrow_b = vecto_refcell.borrow();
+                        let ref_borrow_c = vecto_refcell.borrow();
+                        debug!(?ref_borrow_a);
+                        debug!(?ref_borrow_b);
+                        debug!(?ref_borrow_c);
+                }
+                let span = span!(Level::INFO, "RefCell -- Muttable Borrow, Exclusive");
+                {
+                        let _guard = span.enter();
+                        warn!(
+                                "NOTE: we had to force scope to drop immutable borrows, BEFORE, taking immutable borrow.  No non-lexical scope inference."
+                        );
+                        let ref_mutborrow = vecto_refcell.borrow_mut();
+                        debug!(?ref_mutborrow);
+                }
+        }
 }
 
 #[instrument]
@@ -40,7 +94,7 @@ fn _refcell_example()
                   collections::HashMap,
                   rc::Rc};
 
-        event!(Level::INFO, "create shared map");
+        debug!("create shared map");
         let shared_map: Rc<RefCell<_>> = Rc::new(RefCell::new(HashMap::new()));
         // Create a new block to limit the scope of the dynamic borrow
         {
@@ -55,9 +109,12 @@ fn _refcell_example()
         // Note that if we had not let the previous borrow of the cache fall out
         // of scope then the subsequent borrow would cause a dynamic thread panic.
         // This is the major hazard of using `RefCell`.
-        let total: i32 = shared_map.borrow().values().sum();
-        event!(Level::INFO, "total: {total}");
-        event!(Level::INFO, "map: {:?}", shared_map.borrow());
+        let total: i32 = shared_map
+                .borrow()
+                .values()
+                .sum();
+        debug!("total: {total}");
+        debug!("map: {:?}", shared_map.borrow());
 
         // Create a new block to limit the scope of the dynamic borrow
         {
@@ -72,9 +129,12 @@ fn _refcell_example()
         // Note that if we had not let the previous borrow of the cache fall out
         // of scope then the subsequent borrow would cause a dynamic thread panic.
         // This is the major hazard of using `RefCell`.
-        event!(Level::INFO, "old total: {total}");
-        event!(Level::INFO, "old map: {:?}", shared_map.borrow());
-        let total: i32 = shared_map.borrow().values().sum();
-        event!(Level::INFO, "total: {total}");
-        event!(Level::INFO, "map: {:?}", shared_map.borrow());
+        debug!("old total: {total}");
+        debug!("old map: {:?}", shared_map.borrow());
+        let total: i32 = shared_map
+                .borrow()
+                .values()
+                .sum();
+        debug!("total: {total}");
+        debug!("map: {:?}", shared_map.borrow());
 }
