95479300f4ca397c6370d4a58a02ef236720b964
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-30 13:32:11 +0100

    chore: noting no_mangle as unsafe (edition2024)
    Commentd out; just there for dropping into godbolt

--- samples_conversion/src/main.rs
+++ samples_conversion/src/main.rs
@@ -17,60 +17,60 @@ use tracing::{Level, debug, error, info, info_span, span, trace, warn};
 type Error = Box<dyn core::error::Error>;
 type Result<T> = core::result::Result<T, Error>;
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn square(num: i32) -> i32
 {
         num * num
 }
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_same(i1: i32, i2: i32) -> i32
 {
         i1 * i2
 }
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_as(i1: i32, u: u32) -> i32
 {
         let i2 = u as i32;
         i1 * i2
 }
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_into_easy(i1: i32, ismall: i16) -> i32
 {
         let i2: i32 = ismall.into();
         i1 * i2
 }
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_from_easy(i1: i32, ismall: i16) -> i32
 {
         let i2 = i32::from(ismall);
         i1 * i2
 }
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_into(i1: i32, u: u32) -> i32
 {
         let i2: i32 = u.try_into().unwrap();
         i1 * i2
 }
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_from(i1: i32, u: u32) -> i32
 {
         let i2 = i32::try_from(u).unwrap();
         i1 * i2
 }
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_into_res(i1: i32, u: u32) -> Result<i32>
 {
         let i2: i32 = u.try_into()?;
         Ok(i1 * i2)
 }
 
-// #[no_mangle]
+// #[unsafe(no_mangle)]
 pub fn mult_from_res(i1: i32, u: u32) -> Result<i32>
 {
         let i2 = i32::try_from(u)?;
