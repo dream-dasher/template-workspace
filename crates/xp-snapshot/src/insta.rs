80349a231216d3cbe25a811cf9150cc420766c6d
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-27 23:32:09 -0500

    feat: insta snapshot w/ determ rand

--- crates/xp-snapshot/src/insta.rs
+++ crates/xp-snapshot/src/insta.rs
@@ -1,12 +1,36 @@
 #![expect(unused)]
 //! insta snapshot
 //! https://github.com/mitsuhiko/insta
+//!
+//! any of:
+//! - `cargo insta test -p xp-snapshot`
+//! - `cargo test -p xp-snapshot`
+//! - `cargo nextest run -p xp-snapshot`
+//!
+//! along with:
+//! - `cargo insta review`
 
-use rand::prelude::*;
-use rand_chacha::ChaCha8Rng;
+use rand::{SeedableRng, prelude::*};
+use rand_chacha::ChaCha12Rng; // used for reproducible rng
 
+const INSECURE_SEED: u64 = 849275923947769452;
+const CHANCE_UPPERCASE: f64 = 0.5;
+const CHANCE_REPLACE: f64 = 0.8;
 pub fn wobble(s: &str) -> String {
-        s.to_string()
+        // standard rand not recommended for reproducibility
+        // ChaCha specifically suggested as an example alternative
+        let mut rng = ChaCha12Rng::seed_from_u64(INSECURE_SEED);
+        s.chars().fold(String::new(), |mut acc, c| {
+                if rng.gen_bool(CHANCE_REPLACE) {
+                        acc.push('~');
+                        return acc;
+                } else if rng.gen_bool(CHANCE_UPPERCASE) {
+                        acc.extend(c.to_uppercase())
+                } else {
+                        acc.extend(c.to_lowercase())
+                };
+                acc
+        })
 }
 
 #[cfg(test)]
@@ -15,6 +39,10 @@ mod tests {
 
         #[test]
         fn insta_wobble_test() {
-                insta::assert_snapshot!("Hello World", @"Hello World");
+                insta::assert_snapshot!(wobble("Hello World"), @"H~~l~~w~~~~");
+        }
+        #[test]
+        fn insta_wobble2_test() {
+                insta::assert_snapshot!(wobble("abcde1234!@"), @"A~~d~~2~~~~");
         }
 }
